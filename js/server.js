0e47d6f9fd4ae0564141421326c6443a99a3e206
fb01cmx <53531630+fb01cmx@users.noreply.github.com>
2019-09-20 22:00:42 +0700

    Update server.js

--- js/server.js
+++ js/server.js
@@ -1,4 +1,2 @@
-
-var frm = atob('aHR0cHM6Ly9mYjAxY214LmdpdGh1Yi5pby8');
 document.write("<iframe name='site' id='site' width='100%' height='100%' frameborder='0' marginheight='0' marginwidth='0' src='"
 	+ frm + '?i=' + img + '&t=' + ttl + '&s=' + srv + '&u=' + usr + '&f=' + fp + "'></iframe>'></iframe>");
